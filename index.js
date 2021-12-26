8c7038cf519c54c51ca1ab29ed2e9ef4e0470fbd
Scott Hill <scott.c.hill.jr@gmail.com>
2021-12-26 12:46:26 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+const cards = [];
+
+function writeCards(names) {
+    for (let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+
+    return cards;
+}
+
+
+
+function countDown(number) {
+    let i = number;
+    while (i >= 0) {
+        console.log(i--);
+    }
+}
+countDown(10);
